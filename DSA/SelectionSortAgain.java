6711fec1f36c851080d3feba992960f2a8d6b8bc
Harsh Patil <harshpatil3452@gmail.com>
2026-07-11 15:55:41 +0530

    selection sort from memory

---
+++ DSA/SelectionSortAgain.java
@@ -0,0 +1,29 @@
+public class SelectionSortAgain {
+    public static void main(String[] args){
+        int[] arr = {29, 10, 14, 37, 13, 10, 25, 5};
+        selectionSort(arr);
+        for(int n : arr){
+            System.out.print(n + " ");
+        }
+    }
+
+    private static void selectionSort(int[] arr) {
+
+        for(int i = 0; i< arr.length; i++){
+            int minElementIndex = i;
+            for(int j = i+1; j<arr.length; j++){
+                if(arr[j]<arr[minElementIndex]){ // find the minimum in the entire array.
+                    minElementIndex = j;
+                }
+            }
+            if(minElementIndex != i){ // avoid unnecessary swaps
+                swap(arr,i,minElementIndex);
+            }
+        }
+    }
+    public static void swap(int[] arr, int j, int k){
+        int temp = arr[j];
+        arr[j] = arr[k];
+        arr[k] = temp;
+    }
+}
