c883c2d933d482c2a6a2190ba2e9cb64ebe6b318
Harsh Patil <harshpatil3452@gmail.com>
2026-07-29 17:31:00 +0530

    Added UserAlreadyExistException Handler

---
+++ NotesApp/src/main/java/com/harsh/notesapp/exception/UserAlreadyExistException.java
@@ -0,0 +1,7 @@
+package com.harsh.notesapp.exception;
+
+public class UserAlreadyExistException extends RuntimeException {
+    public UserAlreadyExistException(String message) {
+        super(message);
+    }
+}
